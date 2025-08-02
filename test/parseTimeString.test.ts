029e5f0fccfacc0a4b404b829d9543818e300a09
Jun Kato <i@junkato.jp>
2025-08-02 13:18:58 +0900

    Add TypeScript test configuration

---
+++ test/parseTimeString.test.ts
@@ -0,0 +1,7 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+import { parseTimeString } from '../dist/index.js';
+
+test('parseTimeString converts time strings to seconds', () => {
+  assert.strictEqual(parseTimeString('01:02.03'), 62.03);
+});
