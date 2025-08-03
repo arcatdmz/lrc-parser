74db01e2ac91951c417e3ba5b26c97f6d889a945
Jun Kato <i@junkato.jp>
2025-08-04 04:15:51 +0900

    Minor fix

--- src/parseChars.ts
+++ src/parseChars.ts
@@ -3,28 +3,29 @@ import { ParsedChar } from "./ParsedChar";
 import { parseWords } from "./parseWords";
 
 export function parseChars(items: LRCItem[], length?: number): ParsedChar[][] {
-  const words = parseWords(items, length);
-  return words.map((lineWords) => {
+  const lines = parseWords(items, length);
+  return lines.map((words) => {
     const chars: ParsedChar[] = [];
-    for (const word of lineWords) {
+    for (const word of words) {
       const { word: text, startTime, endTime, line } = word;
       const length = text.length;
-      if (length === 0) continue;
-
+      if (length === 0) {
+        continue;
+      }
       if (length === 1) {
         chars.push({ char: text, startTime, endTime, line });
-      } else {
-        const duration = endTime - startTime;
-        for (let i = 0; i < length; i++) {
-          const charStart = startTime + (duration * i) / length;
-          const charEnd = startTime + (duration * (i + 1)) / length;
-          chars.push({
-            char: text[i],
-            startTime: charStart,
-            endTime: charEnd,
-            line
-          });
-        }
+        continue;
+      }
+      const duration = endTime - startTime;
+      for (let i = 0; i < length; i++) {
+        const charStart = startTime + (duration * i) / length;
+        const charEnd = startTime + (duration * (i + 1)) / length;
+        chars.push({
+          char: text[i],
+          startTime: charStart,
+          endTime: charEnd,
+          line
+        });
       }
     }
     return chars;
